5f1e722352342b4d21caa841c7a27c646097ee96
Chaya Swaroop <52211223+swaroop619@users.noreply.github.com>
2025-06-30 07:22:44 +0000

    new

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 # Perform addition
-result = num1 + num2
+result = num1 - num2
 
 # Display the result
 print("The sum of", num1, "and", num2, "is:", result)
